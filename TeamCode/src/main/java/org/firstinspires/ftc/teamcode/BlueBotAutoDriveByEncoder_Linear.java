645deea080546d81bc2d128bb203b250db8ffe43
piguy21 <72943794+piguy21@users.noreply.github.com>
2022-01-14 21:43:08 -0500

    Things Changed during 1/14/22 meeting FINAL.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBotAutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBotAutoDriveByEncoder_Linear.java
@@ -5,10 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Pushbot: Auto Drive By Encoder Blue", group="Pushbot")
 public class BlueBotAutoDriveByEncoder_Linear extends LinearOpMode {
 
+    static final double     COUNTS_PER_MOTOR_REV    = 1120;
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0;
+    static final double     WHEEL_DIAMETER_INCHES   = 4.5;
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor rightFront;
     private DcMotor leftFront;
     private DcMotor rightRear;
@@ -22,6 +29,8 @@ public class BlueBotAutoDriveByEncoder_Linear extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+
+
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
@@ -35,23 +44,72 @@ public class BlueBotAutoDriveByEncoder_Linear extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
         leftRear.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
         Rightgrabberservo.setDirection(Servo.Direction.REVERSE);
         Leftgrabberservo.setDirection(Servo.Direction.FORWARD);
 
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         waitForStart();
 
-        while (opModeIsActive()) {
-            Rightgrabberservo.setPosition(0);
-            Leftgrabberservo.setPosition(0);
+        Rightgrabberservo.setPosition(0);
+        Leftgrabberservo.setPosition(0);
 
-            rightFront.setPower(-0.5);
-            leftFront.setPower(-0.5);
-            leftRear.setPower(-0.5);
-            rightRear.setPower(-0.5);
+        driveUsingEncoder(0.3, -1.5, -1.5, 30);
+
+
+    }
+    public void driveUsingEncoder(double speed, double leftInches, double rightInches, double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        if (opModeIsActive()) {
+            newLeftTarget = leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftFront.setTargetPosition(newLeftTarget);
+            rightFront.setTargetPosition(newRightTarget);
+            rightRear.setTargetPosition(newRightTarget);
+            leftRear.setTargetPosition(newLeftTarget);
+
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            rightFront.setPower(Math.abs(speed));
+            leftFront.setPower(Math.abs(speed));
+            rightRear.setPower(Math.abs(speed));
+            leftRear.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftFront.isBusy() && rightFront.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        leftFront.getCurrentPosition(),
+                        rightFront.getCurrentPosition());
+                telemetry.update();
+            }
+
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            leftRear.setPower(0);
+            rightRear.setPower(0);
+
+            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
